6d9429f52a0c73b569ce217fbc3604c78656b21c
Your Name <mandy@LAPTOP-GOEDIBSN.localdomain>
2022-09-22 18:27:23 -0600

    Part 1 is complete

---
+++ db/index.js
@@ -0,0 +1,37 @@
+const { Client } = require('pg');
+
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+module.exports = { 
+    client, createUser
+}
+
+async function createUser({ username, password }) {
+    try {
+        const { rows } = await client.query(`
+        INSERT INTO users(username, password) 
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING *;
+        `, [ "some_name", "some_password"]);
+    
+        return rows
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+        `SELECT id, username
+        FROM users;
+        `);
+    
+    return rows;
+}
+
+module.exports = {
+    client,
+    getAllUsers,
+    createUser
+}
